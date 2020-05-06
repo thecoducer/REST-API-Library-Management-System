607b3f35e447dd6c79630e7a4014618ffcf53a6a
Mayukh Datta <mayukh5741@gmail.com>
2020-05-06 22:46:35 +0530

    added application.properties to resources folder

---
+++ src/main/java/io/github/thecoducer/restapilms/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package io.github.thecoducer.restapilms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Hello world";
+	}
+}
