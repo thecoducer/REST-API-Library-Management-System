2d120dbce69c48ec6da4d67a2a802c6ebfdb2fff
Mayukh Datta <mayukh5741@gmail.com>
2020-05-08 03:35:00 +0530

    added swagger & homepage

--- src/main/java/io/github/thecoducer/restapilms/RestApiLmsApplication.java
+++ src/main/java/io/github/thecoducer/restapilms/RestApiLmsApplication.java
@@ -1,13 +1,47 @@
 package io.github.thecoducer.restapilms;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestApiLmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiLmsApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("io.github.thecoducer.restapilms")).paths(PathSelectors.any())
+				.build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Library Manager API", "REST API for Library Management System", "v1", "",
+				new Contact("", "", ""), "", "", Collections.emptyList());
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().docExpansion(DocExpansion.LIST) // or DocExpansion.NONE or
+																				// DocExpansion.FULL
+				.build();
 	}
 
 }
--- src/main/java/io/github/thecoducer/restapilms/controllers/HomeController.java
+++ src/main/java/io/github/thecoducer/restapilms/controllers/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "Hello world";
+		return "home";
 	}
 }
