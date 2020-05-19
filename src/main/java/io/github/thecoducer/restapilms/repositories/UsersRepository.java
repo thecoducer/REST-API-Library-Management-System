b7d9ce054f7de4ae00a192e3654f3d51fc2198d0
Mayukh Datta <mayukh5741@gmail.com>
2020-05-19 22:20:23 +0530

    working on register APIs

--- src/main/java/io/github/thecoducer/restapilms/repositories/UsersRepository.java
+++ src/main/java/io/github/thecoducer/restapilms/repositories/UsersRepository.java
@@ -14,13 +14,13 @@ public interface UsersRepository extends JpaRepository<Users, Integer>{
 	Optional<Users> findByUserName(String userName);
 	Optional<Users> findByEmail(String email);
 	
-	@Query(value = "select * from users where enabled = 1", nativeQuery = true)
+	@Query("SELECT u FROM Users u WHERE u.enabled = 1")
 	List<Users> findAllActiveUsers();
 	
-	@Query(value = "select * from users where enabled = 0", nativeQuery = true)
+	@Query("SELECT u FROM Users u WHERE u.enabled = 0")
 	List<Users> findAllDisabledUsers();
 	
-	@Query(value = "select * from users", nativeQuery = true)
+	@Query("SELECT u FROM Users u")
 	List<Users> showAllUsers();
 	
 }
